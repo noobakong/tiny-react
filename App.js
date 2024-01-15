980f2e71dc54223463c741469695667be7a02824
akong <noobakong@163.com>
2024-01-15 16:34:32 +0800

    chore: some file clean change

--- App.js
+++
@@ -1,5 +0,0 @@
-import React from './core/React.js'
-
-const App = React.createElement('div', { id: "app" }, "hello,", "sss")
-
-export default App
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import ReactDom from './core/ReactDom.js'
-import App from './App.js'
+import App from './App.jsx'
 
 ReactDom.createRoot(document.querySelector('#root')).render(App)
 
